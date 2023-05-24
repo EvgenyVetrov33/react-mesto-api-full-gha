486d988a358925a0c0acb96f4dd7878f4836310f
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-24 15:36:36 +0300

    Добавляем фронт и бэк

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const errorsHandler = require('./errors/errorsHandler');
+const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { cors } = require('cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+// app.use(cors());
+
+app.use(requestLogger); // подключаем логгер запросов
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+mongoose.connect('mongodb://127.0.0.1/mestodb ', {
+  useNewUrlParser: true,
+});
+
+app.use('/', router);
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors());
+app.use(errorsHandler);
+
+app.listen(PORT, () => {
+  console.log(`Приложение слушает порт: ${PORT}`);
+});
