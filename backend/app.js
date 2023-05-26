551dfac944b8dfba986071e60a40746801cdd964
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-26 23:58:36 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const errorsHandler = require('./errors/errorsHandler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const { cors } = require('cors');
+const corsOption = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// app.use(cors());
+app.use(cors(corsOption));
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(bodyParser.json());
