e2fc2075fc928387a86f98b50a1649afb992a992
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-27 18:57:56 +0300

    feat: jwt

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
