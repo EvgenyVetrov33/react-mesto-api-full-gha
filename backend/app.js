2fa9cac542f7ed937a7390a1e934f59c73eb1355
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-27 22:03:24 +0300

    feat: use cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -9,12 +9,12 @@ const { errors } = require('celebrate');
 const errorsHandler = require('./errors/errorsHandler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const mycors = require('./middlewares/cors');
+const mycors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// app.use(mycors);
-app.use(cors());
+app.use(mycors);
+// app.use(cors());
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(bodyParser.json());
