7a13ce13d334ecde0327d6d8b21ecd378a2a0cc9
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-27 16:54:15 +0300

    feat: add cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,17 +1,27 @@
-const CORS_WHITELIST = ['http://localhost:3000',
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const allowedCors = [
+  'http://127.0.0.1:3000',
   'https://selltest.student.nomoredomains.monster',
   'http://selltest.student.nomoredomains.monster',
 ];
 
-const corsOption = {
-  credentials: true,
-  origin: function checkCorsList(origin, callback) {
-    if (CORS_WHITELIST.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-module.exports = corsOption;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next(); // пропускаем запрос дальше
+};
