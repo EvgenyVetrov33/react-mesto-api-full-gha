b5ea97d00126c8ad6d5f7ce903c5c2c24fe94999
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-28 23:17:42 +0300

    feat: restart

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 // const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 // app.use(mycors);
-// app.use(cors());
+app.use(cors());
 
 app.use(requestLogger); // подключаем логгер запросов
 app.use(bodyParser.json());
