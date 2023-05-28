8b6df7da6be91b5d343498f21fb9d42b9cc6adae
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-28 20:02:02 +0300

    feat: cors my del

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,29 +1,29 @@
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept';
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept';
 
-const allowedCors = [
-  'http://127.0.0.1:3000',
-  'https://selltest.student.nomoredomains.monster',
-  'http://selltest.student.nomoredomains.monster',
-];
+// const allowedCors = [
+//   'http://127.0.0.1:3000',
+//   'https://selltest.student.nomoredomains.monster',
+//   'http://selltest.student.nomoredomains.monster',
+// ];
 
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  console.log(origin);
-  const { method } = req;
-  // const requestHeaders = req.headers['access-control-request-headers'];
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   console.log(origin);
+//   const { method } = req;
+//   // const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_HEADERS);
-    return res.end();
-  }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_HEADERS);
+//     return res.end();
+//   }
 
-  next(); // пропускаем запрос дальше
-};
+//   next(); // пропускаем запрос дальше
+// };
