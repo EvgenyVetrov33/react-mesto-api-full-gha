5546131ab79ee0aa5e268396e0e9fca04ee24757
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-28 15:00:07 +0300

    feat: auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,51 @@
-const jwt = require('jsonwebtoken');
-const { AuthError } = require('../errors/index-errors');
+// const jwt = require('jsonwebtoken');
+// const { AuthError } = require('../errors/index-errors');
 
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.jwt;
+
+//   if (!token) {
+//     return next(new AuthError('Токен остутствует или некорректен'));
+//   }
+
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, 'some-secret-key');
+//   } catch (err) {
+//     return next(err);
+//   }
+
+//   req.user = payload;
+
+//   return next();
+// };
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// код для авторизации запроса
+
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/index-errors');
+
+const auth = (req, res, next) => {
+  const { token } = req.cookies;
 
   if (!token) {
     return next(new AuthError('Токен остутствует или некорректен'));
   }
 
   let payload;
-
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(err);
+    next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
   }
 
   req.user = payload;
 
   return next();
 };
+
+module.exports = auth;
