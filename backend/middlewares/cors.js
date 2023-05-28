1914926f352e0d9e3e74fca02629c12131556594
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-28 10:56:29 +0300

    feat: cors *

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,16 +11,16 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   console.log(origin);
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', '*');
     return res.end();
   }
 
