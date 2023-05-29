aeaf101b00bed9403c28d9d29550a65ebf26b8e6
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-29 19:43:09 +0300

    feat: Auth fixx

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,35 +1,8 @@
-const jwt = require('jsonwebtoken');
-const { AuthError } = require('../errors/index-errors');
-
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-
-  if (!token) {
-    return next(new AuthError('Токен остутствует или некорректен'));
-  }
-
-  let payload;
-
-  try {
-    payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
-    return next(err);
-  }
-
-  req.user = payload;
-
-  return next();
-};
-
-// код для авторизации запроса
-
 // const jwt = require('jsonwebtoken');
 // const { AuthError } = require('../errors/index-errors');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
-
-// const auth = (req, res, next) => {
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
 //   const token = req.cookies.jwt;
 
 //   if (!token) {
@@ -37,10 +10,11 @@ module.exports = (req, res, next) => {
 //   }
 
 //   let payload;
+
 //   try {
-//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+//     payload = jwt.verify(token, 'some-secret-key');
 //   } catch (err) {
-//     next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
+//     return next(err);
 //   }
 
 //   req.user = payload;
@@ -48,4 +22,30 @@ module.exports = (req, res, next) => {
 //   return next();
 // };
 
-// module.exports = auth;
+// код для авторизации запроса
+
+const jwt = require('jsonwebtoken');
+const { AuthError } = require('../errors/index-errors');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    return next(new AuthError('Токен остутствует или некорректен'));
+  }
+
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+  } catch (err) {
+    next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
+module.exports = auth;
