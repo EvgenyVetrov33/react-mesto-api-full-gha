8c48b21f3352ca483e0b8e11c3de5bb1622d6515
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-29 21:32:49 +0300

    feat: auth-Authorized

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -24,28 +24,53 @@
 
 // код для авторизации запроса
 
+// const jwt = require('jsonwebtoken');
+// const { AuthError } = require('../errors/index-errors');
+
+// const { NODE_ENV, JWT_SECRET } = process.env;
+
+// const auth = (req, res, next) => {
+//   const token = req.cookies.jwt;
+
+//   if (!token) {
+//     return next(new AuthError('Токен остутствует или некорректен'));
+//   }
+
+//   let payload;
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+//   } catch (err) {
+//     next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
+//   }
+
+//   req.user = payload;
+
+//   return next();
+// };
+
+// module.exports = auth;
+
 const jwt = require('jsonwebtoken');
 const { AuthError } = require('../errors/index-errors');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  let payload;
 
-  if (!token) {
-    return next(new AuthError('Токен остутствует или некорректен'));
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Необходима авторизация');
   }
 
-  let payload;
+  const token = authorization.replace('Bearer ', '');
+
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    next(new AuthError('Токен не верифицирован, авторизация не пройдена'));
+    return next(new AuthError('Необходима авторизация'));
   }
 
   req.user = payload;
-
   return next();
 };
-
-module.exports = auth;
