1bbe23691041710a4d6682766652c4b52c693c44
Evgeny vetrov <sapognaxren@mail.ru>
2023-05-30 18:28:40 +0300

    feat: apiAuth-includes

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,9 @@ const { rateLimiter } = require('./middlewares/rateLimit');
 const errorsHandler = require('./errors/errorsHandler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const mycors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// app.use(mycors);
 app.use(cors());
 app.use(helmet());
 app.use(rateLimiter);
@@ -24,13 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1/mestodb ', {
-	useNewUrlParser: true,
+  useNewUrlParser: true,
 });
 
 app.get('/crash-test', () => {
-	setTimeout(() => {
-		throw new Error('Сервер сейчас упадёт');
-	}, 0);
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
 });
 
 app.use('/', router);
@@ -41,5 +39,5 @@ app.use(errors());
 app.use(errorsHandler);
 
 app.listen(PORT, () => {
-	console.log(`Приложение слушает порт: ${PORT}`);
+  console.log(`Приложение слушает порт: ${PORT}`);
 });
--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -11,7 +11,7 @@ const signUp = (email, password) => {
 	const requestUrl = BASE_URL + '/signup';
 	return fetch(requestUrl, {
 		method: 'POST',
-		credentials: 'include',
+		// credentials: 'include',
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({ email, password }),
 	}).then(checkResponse);
@@ -21,7 +21,7 @@ const signIn = (email, password) => {
 	const requestUrl = BASE_URL + '/signin';
 	return fetch(requestUrl, {
 		method: 'POST',
-		credentials: 'include',
+		// credentials: 'include',
 		headers: { "Content-Type": "application/json" },
 		body: JSON.stringify({ email, password }),
 	}).then(checkResponse);
@@ -31,7 +31,7 @@ const checkToken = (token) => {
 	const requestUrl = BASE_URL + '/users/me';
 	return fetch(requestUrl, {
 		method: 'GET',
-		credentials: 'include',
+		// credentials: 'include',
 		headers: {
 			"Content-Type": "application/json",
 			"Authorization": `Bearer ${token}`
