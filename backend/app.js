f3c3612acd2c88961514e715c3ee725230939aba
Evgeny vetrov <sapognaxren@mail.ru>
2023-06-01 17:49:05 +0300

    fix: authAdd

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const errorsHandler = require('./errors/errorsHandler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -42,6 +43,7 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
+app.use(auth);
 app.use('/', router);
 
 app.use(errorLogger); // подключаем логгер ошибок
