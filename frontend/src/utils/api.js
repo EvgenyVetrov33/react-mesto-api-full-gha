1460c36b11fa162b6dae9aad67ff8c726f3ac8ac
Evgeny vetrov <sapognaxren@mail.ru>
2023-06-01 16:27:15 +0300

    fix: api app

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -132,7 +132,6 @@ class Api {
 const api = new Api({
 	baseUrl: 'https://api.selltest.student.nomoredomains.rocks',
 	headers: {
-		// authorization: '56c23e37-2d26-4f99-806c-d72ff6532f25',
 		'Content-Type': 'application/json',
 		'authorization': `Bearer ${localStorage.getItem('token')}`,
 	},
