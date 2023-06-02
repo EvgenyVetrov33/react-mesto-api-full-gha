b14355cd57ed52aff32e2478169f18b80dc290c8
Evgeny vetrov <sapognaxren@mail.ru>
2023-06-03 00:57:38 +0300

    fix: jwtHeaders

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 class Api {
 	constructor(options) {
 		this._baseUrl = options.baseUrl;
-		this._headers = options.headers;
+		// this._headers = options.headers;
 	}
 
 	_parseResponse(res) {
@@ -13,17 +13,25 @@ class Api {
 
 	// Получение карточек с сервера
 	setInitialCards() {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/cards`, {
-			headers: this._headers
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			}
 		})
 			.then(res => this._parseResponse(res));
 	}
 
 	// Добавление новой карточки через попап
 	addCard(data) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/cards`, {
 			method: 'POST',
-			headers: this._headers,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			},
 			body: JSON.stringify({
 				name: data.name,
 				link: data.link
@@ -34,53 +42,77 @@ class Api {
 
 	// Удаление карточки
 	deleteCard(cardId) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/cards/${cardId}`, {
 			method: 'DELETE',
-			headers: this._headers
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			}
 		})
 			.then(res => this._parseResponse(res));
 	}
 
 	// Ставим лайк карточке
 	setLike(cardId) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
 			method: 'PUT',
-			headers: this._headers
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			}
 		})
 			.then(res => this._parseResponse(res));
 	}
 
 	// Удаляем лайк
 	deleteLike(cardId) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
 			method: 'DELETE',
-			headers: this._headers
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			}
 		})
 			.then(res => this._parseResponse(res));
 	}
 
 	// Получение информации о пользователе с сервера
 	getUserInfo() {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/users/me`, {
-			headers: this._headers
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			}
 		})
 			.then(res => this._parseResponse(res));
 	}
 
 	// Редактирование информации о пользователе через попап
 	editUserInfo(data) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/users/me`, {
 			method: 'PATCH',
-			headers: this._headers,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			},
 			body: JSON.stringify(data),
 		}).then(res => this._parseResponse(res));
 	}
 
 	// Редактирование аватара пользователя через попап
 	editAvatar(data) {
+		const jwt = localStorage.getItem("jwt");
 		return fetch(`${this._baseUrl}/users/me/avatar`, {
 			method: 'PATCH',
-			headers: this._headers,
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${jwt}`,
+			},
 			body: JSON.stringify({
 				avatar: data.avatar
 			})
