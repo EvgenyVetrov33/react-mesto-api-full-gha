2e0965c3f7bedccbda20e97ffde5ea8c7aec2d7d
Evgeny vetrov <sapognaxren@mail.ru>
2023-06-08 12:52:37 +0300

    fix: AppAuth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,47 @@
+// const jwt = require('jsonwebtoken');
+// const { AuthError } = require('../errors/index-errors');
+
+// const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env;
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+//   let payload;
+
+//   if (!authorization || !authorization.startsWith('Bearer ')) {
+//     return next(new AuthError('Необходима авторизация'));
+//   }
+
+//   const token = authorization.replace('Bearer ', '');
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+//   } catch (err) {
+//     return next(new AuthError('Необходима авторизация'));
+//   }
+
+//   req.user = payload;
+//   return next();
+// };
+
 const jwt = require('jsonwebtoken');
 const { AuthError } = require('../errors/index-errors');
 
-const { NODE_ENV, JWT_SECRET = 'some-secret-key' } = process.env;
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  let payload;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthError('Необходима авторизация'));
+  if (!token) {
+    throw new AuthError('Требуется авторизация');
   }
 
-  const token = authorization.replace('Bearer ', '');
+  let payload;
+
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(err);
   }
 
   req.user = payload;
+
   return next();
 };
